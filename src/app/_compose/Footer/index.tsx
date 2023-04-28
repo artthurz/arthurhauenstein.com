9ca9d6fd4c2722155147bd83160c777a30784b1f
artthurz <arthur.hauenstein@universo.univates.br>
2023-04-28 11:43:50 -0300

    fix themeSelector

--- src/app/_compose/Footer/index.tsx
+++ src/app/_compose/Footer/index.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <footer className="pb-16 text-sm px-4 sm:px-6 md:px-8">
+    <footer className="bg-white dark:bg-slate-900 pb-16 text-sm px-4 sm:px-6 md:px-8">
       <div className="max-w-7xl mx-auto divide-y mt-16 border-t border-t-slate-200 dark:border-t-slate-700">
         <div className="pt-10 divide-y ">
           <LogoIcon className="w-auto h-5" />
