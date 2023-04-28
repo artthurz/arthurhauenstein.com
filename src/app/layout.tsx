bc77c695e6f8cca6ee18724b4fe63b1b308869ef
artthurz <arthur.hauenstein@universo.univates.br>
2023-04-27 21:18:39 -0300

    fix themeSelector

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
-"use client";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { Header, Footer } from "./_compose";
-import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
