f58b52fdffe22400ad89d7e921859b7dc74b4815
Arthur Hauenstein <52018251+artthurz@users.noreply.github.com>
2023-05-01 15:16:12 +0000

    Chore cards

--- src/app/_compose/Card/index.tsx
+++ src/app/_compose/Card/index.tsx
@@ -50,6 +50,7 @@ export const Card = ({ children, shadowColor, title, ...props }: CardProps) => {
       transform: `scale(1.1) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`,
       boxShadow: `
           ${-rotateY / 2}px ${rotateX / 2}px 15px ${shadowColor}`,
+      transition: "transform 0s, box-shadow 0s",
     };
 
     const childrenStyle = {
@@ -97,17 +98,23 @@ export const Card = ({ children, shadowColor, title, ...props }: CardProps) => {
   return (
     <div
       style={{
+        transition: "box-shadow 0.6s, transform 0.6s",
         ...cardPerspective,
       }}
       {...props}
-      className="relative backdrop-blur dark:backdrop-blur bg-opacity-50 dark:bg-opacity-50 flex pd-2 w-28 h-28 flex-col md:p-4 rounded-xl md:w-60 md:h-28 md:flex-row overflow-hidden justify-center items-center z-40 touch-none hover:cursor-pointer hover:[transition:transform 0s,box-shadow 0s]"
+      className="relative backdrop-blur dark:backdrop-blur bg-opacity-50 dark:bg-opacity-50 flex pd-2 w-28 h-28 flex-col lg:p-4 rounded-xl lg:w-60 lg:h-28 lg:flex-row overflow-hidden justify-center items-center z-10 touch-none hover:cursor-pointer"
     >
-      <div ref={wrapperRef} className="absolute z-20 inset-0 cursor-pointer" />
+      <div ref={wrapperRef} className="absolute z-10 inset-0 cursor-pointer" />
       <div style={childrenPerspective}>{children}</div>
-      <h1 className="ml-4 dark:text-white text-slate-900">{title}</h1>
+      <h1 className="lg:ml-4 dark:text-white text-slate-900">{title}</h1>
       <div
-        style={highlightPerspective}
-        className="absolute pointer-events-none block [left:-50%] [right:-50%] [width:200%] [height:200%] z-0"
+        style={{
+          transform: "translate3d(0px, 0px, 0px)",
+          transitionDuration: "600ms",
+          opacity: 0,
+          ...highlightPerspective,
+        }}
+        className="radial-gradiant-highlight absolute pointer-events-none block [left:-50%] [right:-50%] [width:200%] [height:200%] z-0"
       />
     </div>
   );
