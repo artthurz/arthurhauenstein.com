7af12d112c30f64e86f6b6241b2d95d43adefa4d
artthurz <arthur.hauenstein@universo.univates.br>
2023-12-28 17:14:33 -0300

    fix: footer

--- src/components/root/Footer/index.tsx
+++ src/components/root/Footer/index.tsx
@@ -6,7 +6,7 @@ export function Footer() {
     <footer className="bg-white dark:bg-slate-900 pb-16 text-sm px-4 sm:px-6 md:px-8">
       <div className="max-w-7xl mx-auto divide-y mt-16 border-t border-t-slate-200 dark:border-t-slate-700">
         <div className="flex justify-between pt-10 divide-y ">
-          <LogoIcon className="w-auto h-5" /> © 2023 from Arthur Hauenstein
+          <LogoIcon className="w-auto h-5" /> © 2023
         </div>
       </div>
     </footer>
