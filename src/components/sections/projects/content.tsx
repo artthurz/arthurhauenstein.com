7ebc93059d5d6897b17fcf38609c2c811736b541
Arthur <arthur.hauenstein@universo.univates.br>
2026-03-16 15:17:11 -0300

    reorder projects

--- src/components/sections/projects/content.tsx
+++ src/components/sections/projects/content.tsx
@@ -3,35 +3,35 @@
 import { ProjectCard } from "./project-card";
 
 interface ProjectDef {
-  code: string;
-  image: string;
-  link: string;
+	code: string;
+	image: string;
+	link: string;
 }
 
 const projects: ProjectDef[] = [
-  {
-    code: "arthurhauenstein",
-    image: "/images/projects/arthurhauenstein.png",
-    link: "https://arthurhauenstein.com",
-  },
-  {
-    code: "carraroplastica",
-    image: "/images/projects/carraroplastica.png",
-    link: "https://carraroplastica.com",
-  },
-  {
-    code: "spironello",
-    image: "/images/projects/spironello.png",
-    link: "https://spironello.com",
-  },
+	{
+		code: "spironello",
+		image: "/images/projects/spironello.png",
+		link: "https://spironello.com",
+	},
+	{
+		code: "carraroplastica",
+		image: "/images/projects/carraroplastica.png",
+		link: "https://carraroplastica.com",
+	},
+	{
+		code: "arthurhauenstein",
+		image: "/images/projects/arthurhauenstein.png",
+		link: "https://arthurhauenstein.com",
+	},
 ];
 
 export function Content() {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full pb-16">
-      {projects.map((project, index) => (
-        <ProjectCard key={project.code} index={index} {...project} />
-      ))}
-    </div>
-  );
+	return (
+		<div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full pb-16">
+			{projects.map((project, index) => (
+				<ProjectCard key={project.code} index={index} {...project} />
+			))}
+		</div>
+	);
 }
